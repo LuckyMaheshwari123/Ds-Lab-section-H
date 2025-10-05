d1b6082b00698b4f88a72887d0b2b3ac4d22f036
LA Maheshwari <lamaheshwari9@gmail.com>
2025-10-05 22:20:09 +0500

    Update task6.cpp

--- lab6/task6.cpp
+++ lab6/task6.cpp
@@ -1,81 +1,96 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
 #include <cmath>
 #include <cctype>
 using namespace std;
 
-int precedence(char c) {
-    if (c == '^') return 3;
-    if (c == '*' || c == '/') return 2;
-    if (c == '+' || c == '-') return 1;
+class CharStack {
+    char arr[100];
+    int top;
+public:
+    CharStack(){top=-1;}
+    void push(char x){arr[++top]=x;}
+    char pop(){return arr[top--];}
+    char peek(){return arr[top];}
+    bool empty(){return top==-1;}
+};
+
+class IntStack {
+    int arr[100];
+    int top;
+public:
+    IntStack(){top=-1;}
+    void push(int x){arr[++top]=x;}
+    int pop(){return arr[top--];}
+};
+
+int prec(char c){
+    if(c=='^') return 3;
+    if(c=='*'||c=='/') return 2;
+    if(c=='+'||c=='-') return 1;
     return 0;
 }
 
-bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+bool isOp(char c){
+    return c=='+'||c=='-'||c=='*'||c=='/'||c=='^';
 }
 
-string infixToPrefix(string infix) {
-    reverse(infix.begin(), infix.end());
-    for (int i = 0; i < infix.size(); i++) {
-        if (infix[i] == '(') infix[i] = ')';
-        else if (infix[i] == ')') infix[i] = '(';
+void reverse(string &s){
+    int n=s.size();
+    for(int i=0;i<n/2;i++) swap(s[i],s[n-1-i]);
+}
+
+string infixToPrefix(string infix){
+    reverse(infix);
+    for(int i=0;i<infix.size();i++){
+        if(infix[i]=='(') infix[i]=')';
+        else if(infix[i]==')') infix[i]='(';
     }
-    stack<char> st;
-    string result;
-    for (char c : infix) {
-        if (isspace(c)) continue;
-        if (isalnum(c)) result += c;
-        else if (c == '(') st.push(c);
-        else if (c == ')') {
-            while (!st.empty() && st.top() != '(') {
-                result += st.top();
-                st.pop();
-            }
+    CharStack st;
+    string res="";
+    for(char c: infix){
+        if(isspace(c)) continue;
+        if(isalnum(c)) res+=c;
+        else if(c=='(') st.push(c);
+        else if(c==')'){
+            while(!st.empty() && st.peek()!='(') res+=st.pop();
             st.pop();
-        } else if (isOperator(c)) {
-            while (!st.empty() && precedence(st.top()) > precedence(c)) {
-                result += st.top();
-                st.pop();
-            }
+        }
+        else if(isOp(c)){
+            while(!st.empty() && prec(st.peek())>prec(c)) res+=st.pop();
             st.push(c);
         }
     }
-    while (!st.empty()) {
-        result += st.top();
-        st.pop();
-    }
-    reverse(result.begin(), result.end());
-    return result;
+    while(!st.empty()) res+=st.pop();
+    reverse(res);
+    return res;
 }
 
-int evaluatePrefix(string prefix) {
-    stack<int> st;
-    for (int i = prefix.size() - 1; i >= 0; i--) {
-        char c = prefix[i];
-        if (isdigit(c)) st.push(c - '0');
-        else if (isOperator(c)) {
-            int a = st.top(); st.pop();
-            int b = st.top(); st.pop();
-            switch (c) {
-                case '+': st.push(a + b); break;
-                case '-': st.push(a - b); break;
-                case '*': st.push(a * b); break;
-                case '/': st.push(a / b); break;
-                case '^': st.push(pow(a, b)); break;
+int evalPrefix(string pre){
+    IntStack st;
+    for(int i=pre.size()-1;i>=0;i--){
+        char c=pre[i];
+        if(isdigit(c)) st.push(c-'0');
+        else if(isOp(c)){
+            int a=st.pop();
+            int b=st.pop();
+            switch(c){
+                case '+': st.push(a+b); break;
+                case '-': st.push(a-b); break;
+                case '*': st.push(a*b); break;
+                case '/': st.push(a/b); break;
+                case '^': st.push(pow(a,b)); break;
             }
         }
     }
-    return st.top();
+    return st.pop();
 }
 
-int main() {
-    string infix;
-    cout << "Enter infix expression: ";
-    getline(cin, infix);
-    string prefix = infixToPrefix(infix);
-    cout << "Prefix expression: " << prefix << endl;
-    cout << "Evaluated result: " << evaluatePrefix(prefix) << endl;
+int main(){
+    string exp;
+    cout<<"Enter infix expression: ";
+    getline(cin,exp);
+    string pre=infixToPrefix(exp);
+    cout<<"Prefix: "<<pre<<endl;
+    cout<<"Result: "<<evalPrefix(pre)<<endl;
     return 0;
 }
