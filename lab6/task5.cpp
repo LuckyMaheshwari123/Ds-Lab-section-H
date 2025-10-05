061e3c02e3e96d6387e708af5825d1dd96dfd59c
LA Maheshwari <lamaheshwari9@gmail.com>
2025-10-05 22:19:35 +0500

    Update task5.cpp

--- lab6/task5.cpp
+++ lab6/task5.cpp
@@ -1,73 +1,84 @@
 #include <iostream>
-#include <stack>
-#include <cctype>
 #include <cmath>
+#include <cctype>
 using namespace std;
 
-int precedence(char op) {
-    if (op == '^') return 3;
-    if (op == '*' || op == '/') return 2;
-    if (op == '+' || op == '-') return 1;
+class Stack {
+    char arr[100];
+    int top;
+public:
+    Stack() { top = -1; }
+    void push(char x) { arr[++top] = x; }
+    char pop() { return arr[top--]; }
+    char peek() { return arr[top]; }
+    bool empty() { return top == -1; }
+};
+
+int precedence(char c) {
+    if (c == '^') return 3;
+    if (c == '*' || c == '/') return 2;
+    if (c == '+' || c == '-') return 1;
     return 0;
 }
 
 bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+    return c=='+'||c=='-'||c=='*'||c=='/'||c=='^';
 }
 
 string infixToPostfix(string infix) {
-    stack<char> st;
-    string postfix = "";
-    for (char c : infix) {
-        if (isspace(c)) continue;
-        if (isalnum(c)) postfix += c;
-        else if (c == '(') st.push(c);
-        else if (c == ')') {
-            while (!st.empty() && st.top() != '(') {
-                postfix += st.top();
-                st.pop();
-            }
+    Stack st;
+    string out="";
+    for(char c: infix){
+        if(isspace(c)) continue;
+        if(isalnum(c)) out+=c;
+        else if(c=='(') st.push(c);
+        else if(c==')'){
+            while(!st.empty() && st.peek()!='('){ out+=st.pop(); }
             st.pop();
-        } else if (isOperator(c)) {
-            while (!st.empty() && precedence(st.top()) >= precedence(c)) {
-                postfix += st.top();
-                st.pop();
-            }
+        } else if(isOperator(c)){
+            while(!st.empty() && precedence(st.peek())>=precedence(c)) out+=st.pop();
             st.push(c);
         }
     }
-    while (!st.empty()) {
-        postfix += st.top();
-        st.pop();
-    }
-    return postfix;
+    while(!st.empty()) out+=st.pop();
+    return out;
 }
 
-int evaluatePostfix(string postfix) {
-    stack<int> st;
-    for (char c : postfix) {
-        if (isdigit(c)) st.push(c - '0');
-        else if (isOperator(c)) {
-            int val2 = st.top(); st.pop();
-            int val1 = st.top(); st.pop();
-            switch (c) {
-                case '+': st.push(val1 + val2); break;
-                case '-': st.push(val1 - val2); break;
-                case '*': st.push(val1 * val2); break;
-                case '/': st.push(val1 / val2); break;
-                case '^': st.push(pow(val1, val2)); break;
+class IntStack {
+    int arr[100];
+    int top;
+public:
+    IntStack(){top=-1;}
+    void push(int x){arr[++top]=x;}
+    int pop(){return arr[top--];}
+    bool empty(){return top==-1;}
+};
+
+int evaluatePostfix(string exp){
+    IntStack st;
+    for(char c: exp){
+        if(isdigit(c)) st.push(c-'0');
+        else if(isOperator(c)){
+            int b=st.pop();
+            int a=st.pop();
+            switch(c){
+                case '+': st.push(a+b); break;
+                case '-': st.push(a-b); break;
+                case '*': st.push(a*b); break;
+                case '/': st.push(a/b); break;
+                case '^': st.push(pow(a,b)); break;
             }
         }
     }
-    return st.top();
+    return st.pop();
 }
 
-int main() {
-    string infix;
-    cout << "Enter infix expression: ";
-    getline(cin, infix);
-    string postfix = infixToPostfix(infix);
-    cout << "Postfix expression: " << postfix << endl;
-    cout << "Evaluated result: " << evaluatePostfix(postfix) << endl;
+int main(){
+    string exp;
+    cout<<"Enter infix expression: ";
+    getline(cin,exp);
+    string post=infixToPostfix(exp);
+    cout<<"Postfix: "<<post<<endl;
+    cout<<"Result: "<<evaluatePostfix(post)<<endl;
     return 0;
 }
