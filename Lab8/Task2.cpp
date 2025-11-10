565f535a5eea2d7753667e8b4036f77c1d25e042
LA Maheshwari <lamaheshwari9@gmail.com>
2025-11-10 22:49:20 +0500

    Update Task2.cpp

--- Lab8/Task2.cpp
+++ Lab8/Task2.cpp
@@ -1,64 +1,68 @@
-//not complete task
-
-
 #include <iostream>
-struct Node{
-    int data;
-    Node*left;
-    Node*right;
-    Node(int value){
-        data=value;
-        left=right=NULL;
-        
-    }
-}
-  
+using namespace std;
 
-Node*insert(Node*root,int data){
-    if(root==NULL){
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int value) {
+        data = value;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) {
         return new Node(data);
     }
-    if(data<root->data){
-        root->left=insert(root->left,data);
+    if (data < root->data) {
+        root->left = insert(root->left, data);
     }
-    else if(data>root->right){
-        root->right=insert(root->right,data);
+    else if (data > root->data) {
+        root->right = insert(root->right, data);
     }
     return root;
 }
-Node*search(Node*root,int data){
-    if(root=NULL || root->data=data){
+
+Node* search(Node* root, int data) {
+    if (root == NULL || root->data == data) {
         return root;
     }
-    if(data<root->data){
-        return search(root->left,data);
-        
+    if (data < root->data) {
+        return search(root->left, data);
     }
-    return search(root>right,data);
-}
-void inorder(Node*root){
-  inorder(root->left);
- cout<<root->data<<" ";
- inorder(root->rigt);
+    return search(root->right, data);
 }
 
-void postorder(Node*root){
-  postorder(root->left);
-   postorder(root->rigt);
-   cout<<root->data<<" ";
-
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
 }
+
+void postorder(Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
 int main() {
-  Node*rootNode=new Node(10);
-  Node*song1=new Node(12);
-  Node*song2=new Node(13);
-  Node*song3=new Node(15);
-  Node*song4=new Node(8);
-  insert(rootNode,song1);
-  insert(rootNode,song2);
-  insert(rootNode,song3);
-  insert(rootNode,song4);
-  inorder(root);
-  
+    Node* rootNode = new Node(10);
+    insert(rootNode, 12);
+    insert(rootNode, 13);
+    insert(rootNode, 15);
+    insert(rootNode, 8);
+    
+    cout << "Inorder Traversal: ";
+    inorder(rootNode);
+    cout << endl;
+
+    cout << "Postorder Traversal: ";
+    postorder(rootNode);
+    cout << endl;
+
     return 0;
 }
